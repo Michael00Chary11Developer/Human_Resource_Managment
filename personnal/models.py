2a017735f4edf473774ba42d51419c5cb3890374
Poriya <pouryachary.8228@gmail.com>
2024-09-09 17:57:10 +0330

    fix:pourya-fix model personnal

--- personnal/models.py
+++ personnal/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+import uuid
 
 
 
 class Personnal(models.Model):
-    number_of_personnal = models.CharField(max_length=100)
+    
+    
+    
+    number_of_personnal = models.CharField(max_length=100,blank=True, null=True, )
     """
         Personnel number    
     """
@@ -39,7 +43,7 @@ class Personnal(models.Model):
     
     
     def ___str__(self):
-        return f"{self.number_of_personnal} - {self.firstname} {self.lastname}" 
+        return f"{self.number_of_personnal} - {self.firstname} - {self.lastname}" 
       
     
       
