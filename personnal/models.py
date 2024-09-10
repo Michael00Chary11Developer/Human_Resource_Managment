c64434f6abd09e5c233160d923c195c1144bdf0a
Poriya <pouryachary.8228@gmail.com>
2024-09-10 14:49:01 +0330

    feat:pourya-add feature for number_of_personnel

--- personnal/models.py
+++ personnal/models.py
@@ -1,13 +1,34 @@
 from django.db import models
-import uuid
+import random
 
 
 
 class Personnal(models.Model):
     
+    def create_new_number_of_personnal():
+        while True:
+            random_number = random.randint(1, 100)
+            costum_format = f"100-{random_number:03d}"
+            
+            if not Personnal.objects.filter(number_of_personnal=costum_format).exists():
+                
+                return costum_format
     
     
-    number_of_personnal = models.CharField(max_length=100,blank=True, null=True, )
+    number_of_personnal = models.CharField(max_length=100,blank=True,editable=False, unique=True, default=create_new_number_of_personnal, db_index=True)
+    # def save(self, *args, **kwargs):
+    #     if not self.number_of_personnal:
+    #         last_person = Personnal.objects.all().order_by('id').last()
+    #         if last_person:
+    #             last_number = int(last_person.number_of_personnal[4:])  
+    #             self.number_of_personnal = f"100-{last_number + 1:03d}"  
+    #         else:
+    #             self.number_of_personnal = "100-001"  
+    #     super().save(*args, **kwargs)
+    
+    
+    
+    
     """
         Personnel number    
     """
@@ -34,7 +55,7 @@ class Personnal(models.Model):
     """
     
     position = models.CharField(max_length=100)
-    level_fo_position = models.CharField(max_length=100)
+    level_for_position = models.CharField(max_length=100)
     """
         position and level
     """
@@ -42,7 +63,7 @@ class Personnal(models.Model):
     Date_of_employment = models.DateField()
     
     
-    def ___str__(self):
+    def __str__(self):
         return f"{self.number_of_personnal} - {self.firstname} - {self.lastname}" 
       
     
