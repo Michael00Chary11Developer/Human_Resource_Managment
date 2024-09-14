38ae0e07be0ba1e1cd78b38d1499876f6d3e1426
Michael Chary <michaelchary081@gmail.com>
2024-09-14 12:52:57 +0330

    fix:michael.add generate code method to utils and use it in models

--- resources/models.py
+++ resources/models.py
@@ -1,9 +1,20 @@
+from .utils import CreateUniqueCode
 from django.db import models
 
 
 class Resources(models.Model):
-    
+
+    asset_code = models.CharField(
+        max_length=8, editable=False, blank=False, primary_key=True)
     resource_name = models.CharField(
-        max_length=20, blank=False, unique=False, editable=False)
-    resource_sort=models.CharField(max_length=2,blank=True)
+        max_length=20, blank=False, unique=False, editable=True)
+    resource_sort = models.CharField(max_length=20, blank=True)
     dateـofـallocation = models.DateField()
+
+    def save(self):
+        if not self.asset_code:
+            self.asset_code = CreateUniqueCode.generate_unique_code(Resources)
+        super().save()
+
+    def __str__(self) -> str:
+        return f'{self.resource_name}\t{self.resource_sort}\t{self.dateـofـallocation}'
--- resources/utils.py
+++ resources/utils.py
@@ -3,11 +3,10 @@ from django.db import models
 
 
 class CreateUniqueCode:
-    @staticmethod
-    def create_code(model_field: models, unique_code: str) -> str:
+
+    def generate_unique_code(self:models.Model):
         while True:
             random_number = randint(1, 100)
-            create_code = f'20000{random_number:05d}'
-
-            if not model_field.objects.filter(unique_code=create_code).exists():
-                return create_code
+            create_number = f'20000{random_number:03d}'
+            if not self.objects.filter(asset_code=create_number).exists():
+                return create_number        
