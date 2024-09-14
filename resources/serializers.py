8827a0a9f4be92c8cf9cd84284bde3c41b98e4b2
Michael Chary <michaelchary081@gmail.com>
2024-09-14 12:54:22 +0330

    fix:michael.fix recource.urls.py and serializer.py lines

--- resources/serializers.py
+++ resources/serializers.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 
 
 class ResourceSerializer(serializers.ModelSerializer):
+    code_of_asset = serializers.SerializerMethodField()
+
     class Meta:
         model = Resources
         fields = '__all__'
-        
--- resources/urls.py
+++ resources/urls.py
@@ -3,7 +3,7 @@ from .views import ResourceView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('resource/', ResourceView)
+router.register('resource', ResourceView)
 
 
 urlpatterns = [
