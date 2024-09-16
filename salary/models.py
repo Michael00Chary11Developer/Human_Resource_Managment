c4e1ee17e533f84968a8efc27411e738a9115f17
Poriya <pouryachary.8228@gmail.com>
2024-09-16 10:17:05 +0330

    register salary model in admin and change model

--- salary/models.py
+++ salary/models.py
@@ -4,7 +4,7 @@ from personnel.models import Personnel
 
 class Salary(models.Model):
     
-    personnel = models.ForeignKey(Personnel, on_delete=models.CASCADE, related_name='sallaries')
+    personnel = models.OneToOneField(Personnel, on_delete=models.CASCADE, related_name='sallaries')
 
     base_salary = (models.DecimalField(max_digits=10, decimal_places=3))
     housing_allowance = (models.DecimalField(max_digits=10, decimal_places=2))
@@ -13,4 +13,4 @@ class Salary(models.Model):
     salary_start_date = (models.DateField())
     
     def __str__(self) :
-        return f"{self.personnel.number_of_personnel} - {self.personnel.firstname} - {self.base_salary}" 
+        return f"{self.personnel.number_of_personnel}  - {self.base_salary}" 
