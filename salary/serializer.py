3c57ae88e7a9cfa34226b2404cceada0e7595783
Michael Chary <michaelchary081@gmail.com>
2024-09-17 09:35:57 +0330

    fix:Michael salary serializer and views fix

--- salary/serializer.py
+++ salary/serializer.py
@@ -3,39 +3,29 @@ from .models import Salary
 from personnel.models import Personnel
 
 
-
-
-
-
 class SalarySerializer(serializers.ModelSerializer):
-    
-    personnel = serializers.PrimaryKeyRelatedField(queryset=Personnel.objects.all())
+
+    personnel = serializers.PrimaryKeyRelatedField(
+        queryset=Personnel.objects.all())
     firstname = serializers.SerializerMethodField()
     lastname = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Salary
         fields = ['personnel',
                   'firstname',
-                  'lastname', 
-                  'base_salary', 
-                  'housing_allowance', 
-                  'child_allowance', 
-                  'food_allowance', 
+                  'lastname',
+                  'base_salary',
+                  'housing_allowance',
+                  'child_allowance',
+                  'food_allowance',
                   'salary_start_date'
                   ]
-        
-        
-        
-    def get_firstname(self, obj):
-        
+
+    def get_firstname(self, obj: Salary) -> str:
+
         return obj.personnel.firstname
-    
-    def get_lastname(self, obj):
-        
+
+    def get_lastname(self, obj: Salary) -> str:
+
         return obj.personnel.lastname
-  
-        
-
-
- 
--- salary/views.py
+++ salary/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from rest_framework.request import Request
 from .models import Salary
 from .serializer import SalarySerializer
-from rest_framework.views import APIView
 
 
 
