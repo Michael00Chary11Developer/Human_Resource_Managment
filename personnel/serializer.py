720a35ba1fb0970e6e419fbfabf976221ccd8078
Michael Chary <michaelchary081@gmail.com>
2024-09-17 12:16:17 +0330

    feat:home.mic.pou.nested serilizer to salary nested

--- personnel/serializer.py
+++ personnel/serializer.py
@@ -3,7 +3,29 @@ from .models import Personnel
 from datetime import datetime
 
 
-class PersonnelSerializer(serializers.ModelSerializer):
+
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class PersonnelSerializer(DynamicFieldsModelSerializer):
     """
     Serializer for the Personnel model.
 
--- salary/serializer.py
+++ salary/serializer.py
@@ -1,31 +1,33 @@
 from rest_framework import serializers
 from .models import Salary
-from personnel.models import Personnel
+from personnel.serializer import DynamicFieldsModelSerializer,PersonnelSerializer
 
 
-class SalarySerializer(serializers.ModelSerializer):
+class SalarySerializer(DynamicFieldsModelSerializer):
 
-    personnel = serializers.PrimaryKeyRelatedField(
-        queryset=Personnel.objects.all())
-    firstname = serializers.SerializerMethodField()
-    lastname = serializers.SerializerMethodField()
+    # personnel = serializers.PrimaryKeyRelatedField(
+    #     queryset=Personnel.objects.all())
+    # firstname = serializers.SerializerMethodField()
+    # lastname = serializers.SerializerMethodField()
+    personnel = PersonnelSerializer(fields=['','firstname','lastname','degree'])
 
     class Meta:
         model = Salary
-        fields = ['personnel',
-                  'firstname',
-                  'lastname',
-                  'base_salary',
-                  'housing_allowance',
-                  'child_allowance',
-                  'food_allowance',
-                  'salary_start_date'
-                  ]
+        # fields = ['personnel',
+        #           'firstname',
+        #           'lastname',
+        #           'base_salary',
+        #           'housing_allowance',
+        #           'child_allowance',
+        #           'food_allowance',
+        #           'salary_start_date'
+        #           ]
+        fields = '__all__'
 
-    def get_firstname(self, obj: Salary) -> str:
+    # def get_firstname(self, obj: Salary) -> str:
 
-        return obj.personnel.firstname
+    #     return obj.personnel.firstname
 
-    def get_lastname(self, obj: Salary) -> str:
+    # def get_lastname(self, obj: Salary) -> str:
 
-        return obj.personnel.lastname
+    #     return obj.personnel.lastname
