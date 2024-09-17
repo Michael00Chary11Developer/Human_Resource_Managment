f3bdd4380b5b6e690421ad6d90ccca6ff68ae8a0
Poriya <pouryachary.8228@gmail.com>
2024-09-17 14:06:09 +0330

    pourya add feat serialize and models salary

--- salary/models.py
+++ salary/models.py
@@ -5,13 +5,15 @@ from personnel.models import Personnel
 class Salary(models.Model):
     
     
-    personnel = models.OneToOneField(Personnel, on_delete=models.CASCADE, related_name='sallaries')
+    personnel = models.OneToOneField(Personnel, on_delete=models.CASCADE, related_name='sallaries', primary_key= True)
 
-    base_salary = (models.DecimalField(max_digits=10, decimal_places=3))
-    housing_allowance = (models.DecimalField(max_digits=10, decimal_places=2))
-    child_allowance = (models.DecimalField(max_digits=10, decimal_places=2))
-    food_allowance= (models.DecimalField(max_digits=10, decimal_places=2))
-    salary_start_date = (models.DateField())
+    base_salary = models.DecimalField(max_digits=10, decimal_places=3)
+    housing_allowance = models.DecimalField(max_digits=10, decimal_places=3)
+    child_allowance = models.DecimalField(max_digits=10, decimal_places=3)
+    food_allowance= models.DecimalField(max_digits=10, decimal_places=3)
+    salary_start_date = models.DateField()
+    
+   
     
     def __str__(self) :
         return f"{self.personnel.number_of_personnel} - {self.personnel.firstname}" 
--- salary/serializer.py
+++ salary/serializer.py
@@ -3,39 +3,54 @@ from .models import Salary
 from personnel.models import Personnel
 
 
+class PersonnelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Personnel
+        fields = ['number_of_personnel','firstname', 'lastname']
 
 
 
 
 class SalarySerializer(serializers.ModelSerializer):
-    
-    personnel = serializers.PrimaryKeyRelatedField(queryset=Personnel.objects.all())
-    firstname = serializers.SerializerMethodField()
-    lastname = serializers.SerializerMethodField()
-    
+
+    # firstname = serializers.SerializerMethodField()
+    # lastname = serializers.SerializerMethodField()
+    personnel = PersonnelSerializer()
     class Meta:
+        
+        
         model = Salary
         fields = ['personnel',
-                  'firstname',
-                  'lastname', 
-                  'base_salary', 
-                  'housing_allowance', 
-                  'child_allowance', 
-                  'food_allowance', 
-                  'salary_start_date'
+                #   'firstname',
+                #   'lastname',
+                  'base_salary',
+                  'housing_allowance',
+                  'child_allowance',
+                  'food_allowance',
+                  'salary_start_date',
                   ]
         
         
+        def create(self, validated_data):
+            personnel_data = validated_data.pop('personnel')
+            
+            personnel = Personnel.objects.get(number_of_personnel = personnel_data['number_of_personnel'])
+            
+            salary = Salary.objects.create(personnel=personnel, **validated_data)
+            
+            return salary
         
-    def get_firstname(self, obj):
         
-        return obj.personnel.firstname
-    
-    def get_lastname(self, obj):
-        
-        return obj.personnel.lastname
-  
-        
+       
+        # def update(self, instance, validated_data):
+        #     personnel_data = validated_data.pop('personnel')
+            
+        #     personnel = Personnel.objects.get(number_of_personnel=personnel_data['number'])
 
+    # def get_firstname(self, obj: Salary) -> str:
 
- 
+    #     return obj.personnel.firstname
+
+    # def get_lastname(self, obj: Salary) -> str:
+
+    #     return obj.personnel.lastname
