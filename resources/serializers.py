45ac1d8406b4884ef12ca20afca05effa95681e9
Michael Chary <michaelchary081@gmail.com>
2024-09-17 13:15:10 +0330

    Revert "feat:serialiers.py in resource change"
    This reverts commit c18dfb7cd05d40a4c813511a69e7900029e04d9d.

--- resources/serializers.py
+++ resources/serializers.py
@@ -14,23 +14,8 @@ serializr class that serialize mean convert json to python object python object 
         Resource and say which model must be serialize         
 """
 
-
 class ResourceSerializer(serializers.ModelSerializer):
-
-    first_name = serializers.SerializerMethodField()
-    last_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Resources
-        fields = ['number_of_personnel', 'first_name',
-                  'last_name', 'asset_code',
-                  'resource_name', 'resource_sort',
-                  'dateـofـallocation']
-
-    def get_first_name(self, obj: Resources):
-        first_name = obj.number_of_personnel.firstname
-        return first_name
-
-    def get_last_name(self, obj: Resources):
-        last_name = obj.number_of_personnel.lastname
-        return last_name
+        fields = '__all__'
