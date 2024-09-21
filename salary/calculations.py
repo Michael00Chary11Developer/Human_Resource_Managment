9155b52e7dfcbe16d6ac2c5e775bf6fd496b5748
PouryaCh <pouryachary.8228@gmail.com>
2024-09-21 22:13:16 +0330

    feat:pourya-add gross and net salary for salary app

---
+++ salary/calculations.py
@@ -0,0 +1,9 @@
+from decimal import Decimal
+
+def calculate_gross_salary(base_salary, housing_allowance, child_allowance, food_allowance):
+        
+    return base_salary + housing_allowance + child_allowance + food_allowance
+
+def calculate_net_salary(gross_salary, tax_rate=Decimal('0.1')):
+
+    return gross_salary * (Decimal(1) - tax_rate)
--- salary/serializer.py
+++ salary/serializer.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 from .models import Salary
 from personnel.models import Personnel
+from .calculations import calculate_gross_salary, calculate_net_salary
+from decimal import Decimal
+
+
 
 
 class PersonnelSerializer(serializers.ModelSerializer):
@@ -17,6 +21,10 @@ class SalarySerializer(serializers.ModelSerializer):
     # lastname = serializers.SerializerMethodField()
     personnel = serializers.PrimaryKeyRelatedField(queryset=Personnel.objects.all(), write_only=True) 
     personnel_detail = PersonnelSerializer(source='personnel', read_only=True)
+
+    gross_salary = serializers.SerializerMethodField(read_only=True)
+    net_salary = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         
         
@@ -30,16 +38,34 @@ class SalarySerializer(serializers.ModelSerializer):
                   'child_allowance',
                   'food_allowance',
                   'salary_start_date',
+                  'gross_salary',
+                  "net_salary"
                   ]
+        
+
+    def get_gross_salary(self, obj: Salary) -> Decimal:
+        return calculate_gross_salary(
+            obj.base_salary, 
+            obj.housing_allowance, 
+            obj.child_allowance, 
+            obj.food_allowance
+        )
+    
+    def get_net_salary(self, obj: Salary) -> Decimal:
+        gross = self.get_gross_salary(obj)
+        return calculate_net_salary(gross)
         
     def validate(self, data):
         personnel = data.get('personnel')
 
+        if self.instance:
+            if self.instance.personnel == personnel:
+                return data
+
         if Salary.objects.filter(personnel=personnel).exists():
             raise serializers.ValidationError(f'A salary record for personnel {personnel} already exists.')
-        
+
         return data
-        
         
         
 
