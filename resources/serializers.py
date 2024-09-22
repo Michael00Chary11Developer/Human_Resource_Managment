c184561ea3129a6eb64a2f0d5cf2ab5ba2b94aa7
Michael Chary <michaelchary081@gmail.com>
2024-09-22 16:04:47 +0330

    fail:michael resource serializer

--- resources/serializers.py
+++ resources/serializers.py
@@ -25,10 +25,24 @@ class PersonnelSerializer(serializers.ModelSerializer):
 
 class ResourceSerializer(serializers.ModelSerializer):
 
-    number_of_personnel_ex = PersonnelSerializer(read_only=True)
+    personnel = serializers.PrimaryKeyRelatedField(queryset=Personnel.objects.all(), write_only=True) 
+    personnel_detail = PersonnelSerializer(
+        read_only=True, source='personnel_number')
 
     class Meta:
         model = Resources
-        fields = ['user_id', 'personnel_number', 'number_of_personnel_ex',
+        fields = ['user_id','personnel' ,'personnel_number', 'personnel_detail',
                   'asset_code', 'resource_name', 'resource_sort', 'dateـofـallocation',
                   'created_at', 'update_at']
+
+    def validate(self, data):
+        personnel = data.get('personnel')
+
+        if self.instance:
+            if self.instance.personnel == personnel:
+                return data
+
+        if Resources.objects.filter(personnel=personnel).exists():
+            raise serializers.ValidationError(f'A Resource record for personnel {personnel} already exists.')
+
+        return data
