98db422f1a4c2314aaffdb1cab372ad0b124e823
Michael Chary <michaelchary081@gmail.com>
2024-09-22 10:52:29 +0330

    change view of salary

--- salary/views.py
+++ salary/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Salary
 from .serializer import SalarySerializer
-from salary.filters import CustomSalaryFilter
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+
 
 class SalaryViewSet(ModelViewSet):
-    
+
     queryset = Salary.objects.all()
     serializer_class = SalarySerializer
-    # filter_backends=[DjangoFilterBackend,filters.SearchFilter]
-    # # filter_backends=[CustomSalaryFilter,]
-    # search_fields=['base_salary']
-    # filterset_fields=['base_salary']
