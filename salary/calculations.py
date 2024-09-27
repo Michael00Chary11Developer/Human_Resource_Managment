7c7869b1c37c1662c10b936affe52265d3b750c0
PouryaCh <pouryachary.8228@gmail.com>
2024-09-27 21:24:38 +0330

    feat:pourya-add documentation for Salary app and update documentation for personnel app

--- salary/calculations.py
+++ salary/calculations.py
@@ -3,6 +3,25 @@ from decimal import Decimal
 
 def calculate_gross_salary(base_salary, housing_allowance, child_allowance, food_allowance, number_of_child, marital_status):
 
+    """
+    Calculate the gross salary based on various allowances and marital status.
+
+    Args:
+        base_salary (Decimal): The basic salary of the personnel.
+        housing_allowance (Decimal): The allowance for housing.
+        child_allowance (Decimal): The allowance for each child.
+        food_allowance (Decimal): The allowance for food.
+        number_of_child (int or None): The number of children the personnel has.
+        marital_status (str): The marital status of the personnel ('single' or 'married').
+
+    Returns:
+        Decimal: The total gross salary calculated.
+
+    Notes:
+        If the marital status is 'single' or if the personnel is 'married' but has no children
+        (i.e., number_of_child is 0 or None), the child allowance is not included in the calculation.
+    """
+
     if marital_status == 'single' or (marital_status == 'married' and number_of_child in [0, None]):
         return base_salary + housing_allowance + food_allowance
 
@@ -10,5 +29,16 @@ def calculate_gross_salary(base_salary, housing_allowance, child_allowance, food
 
 
 def calculate_net_salary(gross_salary, tax_rate=Decimal('0.1')):
+
+    """
+    Calculate the net salary after applying a tax rate.
+
+    Args:
+        gross_salary (Decimal): The total gross salary.
+        tax_rate (Decimal, optional): The tax rate to be applied. Default is 10%.
+
+    Returns:
+        Decimal: The net salary after tax deduction.
+    """
 
     return gross_salary * (Decimal(1) - tax_rate)
--- salary/urls.py
+++ salary/urls.py
@@ -9,3 +9,17 @@ router.register('manage', views.SalaryViewSet, basename='salary')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+"""
+This module contains URL patterns for the salary app.
+
+- Router:
+    - A router is used to automatically generate URL patterns for the SalaryViewSet.
+
+- ViewSet Registration:
+    - SalaryViewSet is registered with the prefix 'manage'.
+    - This ViewSet provides CRUD (Create, Read, Update, and Delete) operations for salary records.
+
+- URL Patterns:
+    - Includes all generated URL patterns for managing salaries.
+"""
